c2c204fce19e1dd69d86573ac2404c2de3630160
Ahmadryan99 <oconk.based99@gmail.com>
2023-06-14 22:34:57 +0700

    feat: :sparkles: add daftar kelompok

--- lib/views/daftar_kelompok.dart
+++ lib/views/daftar_kelompok.dart
@@ -45,19 +45,28 @@ class CustomGridItemWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10), color: Colors.white),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+      ),
+      child: Stack(
+        alignment: Alignment.center,
         children: [
           Image.network(
             "https://picsum.photos/id/${2 + index}/500/500",
+            fit: BoxFit.cover,
           ),
-          SizedBox(height: 10),
-          Text(
-            'Kelompok ${index + 1}',
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
+          Positioned.fill(
+            child: Container(
+              alignment: Alignment.bottomLeft,
+              color: Colors.black.withOpacity(0.4),
+              child: Text(
+                'Kelompok ${index + 1}',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
             ),
           ),
         ],
--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -5,7 +5,10 @@ import 'package:boilerplate_ui/views/loading_screens.dart';
 import 'package:boilerplate_ui/views/nilai_page.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:provider/provider.dart';
+
+import 'daftar_kelompok.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -282,8 +285,9 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           TextButton(
               onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (BuildContext context) => NilaiScreen()));
+                // Navigator.of(context).push(MaterialPageRoute(
+                //     builder: (BuildContext context) => NilaiScreen()));
+                Get.to(NilaiScreen());
               },
               child: Container(
                 padding: EdgeInsets.only(left: 190),
@@ -316,7 +320,10 @@ class _HomeScreenState extends State<HomeScreen> {
                   style: AppTheme.getTextStyle(themeData.textTheme.titleLarge!,
                       fontWeight: 700, color: themeData.colorScheme.secondary)),
               InkWell(
-                  onTap: () {},
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (BuildContext context) => InfoKelompok()));
+                  },
                   child: Text("Lihat semua",
                       style: AppTheme.getTextStyle(
                           themeData.textTheme.bodySmall!,
@@ -346,13 +353,15 @@ class _HomeScreenState extends State<HomeScreen> {
             width: 125,
             height: 155,
             decoration: BoxDecoration(
-                color: themeData.colorScheme.primary.withAlpha(20),
-                borderRadius: BorderRadius.circular(10),
-                image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: AssetImage(
-                      imgCategoryList[index],
-                    ))),
+              color: themeData.colorScheme.primary.withAlpha(20),
+              borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage(
+                  imgCategoryList[index],
+                ),
+              ),
+            ),
             padding: Spacing.all(15),
           ),
           Container(
