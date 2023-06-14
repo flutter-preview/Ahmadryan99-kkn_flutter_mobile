36d180fca0eaba6e9d466665d2658ef09a91b44d
dimas-ibnu <dimasibnumalik@gmail.com>
2023-06-14 23:29:32 +0700

    fix: :ambulance: change statefull login into getview

--- lib/app/modules/login/page/login_page.dart
+++ lib/app/modules/login/page/login_page.dart
@@ -3,91 +3,20 @@ import 'package:boilerplate_ui/app_theme_notifier.dart';
 import 'package:boilerplate_ui/utils/SizeConfig.dart';
 import 'package:boilerplate_ui/views/auth/register_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:provider/provider.dart';
 
 import '../../../../views/app_screen.dart';
+import '../controllers/login_controller.dart';
 
-class LoginScreen extends StatefulWidget {
-  @override
-  _LoginScreenState createState() => _LoginScreenState();
-}
+class LoginScreen extends GetView<LoginController> {
 
-class _LoginScreenState extends State<LoginScreen> {
-  //Theme Data
-  late ThemeData themeData;
-  late CustomAppTheme customAppTheme;
-
-  //Text-Field Controller
-  late TextEditingController emailTFController;
-  late TextEditingController passwordTFController;
-
-  //Global Keys
-  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
-
-  //Other Variables
-  bool isInProgress = false;
-  bool showPassword = false;
-
-  //UI Variables
-  late OutlineInputBorder allTFBorder;
-
-  @override
-  void initState() {
-    super.initState();
-    isInProgress = false;
-    emailTFController = TextEditingController(text: "");
-    passwordTFController = TextEditingController(text: "");
-  }
-
-  @override
-  void dispose() {
-    emailTFController.dispose();
-    passwordTFController.dispose();
-    super.dispose();
-  }
-
-  _initUI() {
-    allTFBorder = OutlineInputBorder(
-        borderRadius: BorderRadius.all(
-          Radius.circular(8),
-        ),
-        borderSide: BorderSide(color: customAppTheme.bgLayer4, width: 1.5));
-  }
-
-  _handleLogin() async {
-    await Future.delayed(Duration(seconds: 1), () {
-      if (mounted) {
-        setState(() {
-          isInProgress = false;
-        });
-      }
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (BuildContext context) => AppScreen(),
-        ),
-      );
-    });
-    // }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Consumer<AppThemeNotifier>(
-      builder: (BuildContext context, AppThemeNotifier value, Widget? child) {
-        int themeType = value.themeMode();
-        themeData = AppTheme.getThemeFromThemeMode(themeType);
-        customAppTheme = AppTheme.getCustomAppTheme(themeType);
-        _initUI();
-        return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            theme: AppTheme.getThemeFromThemeMode(value.themeMode()),
-            home: Scaffold(
+    return Scaffold(
                 resizeToAvoidBottomInset: false,
-                key: _scaffoldKey,
                 body: Container(
-                    color: customAppTheme.bgLayer1,
+                    color: CustomAppTheme().bgLayer1,
                     child: ListView(
                       padding: Spacing.top(100),
                       children: <Widget>[
@@ -116,20 +45,20 @@ class _LoginScreenState extends State<LoginScreen> {
                           margin: Spacing.fromLTRB(24, 24, 24, 0),
                           child: TextFormField(
                             style: AppTheme.getTextStyle(
-                                themeData.textTheme.bodyLarge!,
+                                Get.theme.textTheme.bodyLarge!,
                                 letterSpacing: 0.1,
-                                color: themeData.colorScheme.onBackground,
+                                color: Get.theme.colorScheme.onBackground,
                                 fontWeight: 500),
                             decoration: InputDecoration(
                                 hintText: "Nomer Induk Mahasiswa",
                                 hintStyle: AppTheme.getTextStyle(
-                                    themeData.textTheme.titleSmall!,
+                                    Get.theme.textTheme.titleSmall!,
                                     letterSpacing: 0.1,
-                                    color: themeData.colorScheme.onBackground,
+                                    color: Get.theme.colorScheme.onBackground,
                                     fontWeight: 500),
-                                border: allTFBorder,
-                                enabledBorder: allTFBorder,
-                                focusedBorder: allTFBorder,
+                                border: controller.getOutlineInputBorder,
+                                enabledBorder: controller.getOutlineInputBorder,
+                                focusedBorder: controller.getOutlineInputBorder,
                                 prefixIcon: Icon(
                                   MdiIcons.emailOutline,
                                   size: MySize.size22,
@@ -137,40 +66,38 @@ class _LoginScreenState extends State<LoginScreen> {
                                 isDense: true,
                                 contentPadding: Spacing.zero),
                             keyboardType: TextInputType.emailAddress,
-                            controller: emailTFController,
+                            controller: controller.emailTFController,
                           ),
                         ),
                         Container(
                           margin: Spacing.fromLTRB(24, 16, 24, 0),
                           child: TextFormField(
-                            obscureText: !showPassword,
+                            obscureText: !(controller.showPassword.value),
                             style: AppTheme.getTextStyle(
-                                themeData.textTheme.bodyLarge!,
+                                Get.theme.textTheme.bodyLarge!,
                                 letterSpacing: 0.1,
-                                color: themeData.colorScheme.onBackground,
+                                color: Get.theme.colorScheme.onBackground,
                                 fontWeight: 500),
                             decoration: InputDecoration(
                               hintStyle: AppTheme.getTextStyle(
-                                  themeData.textTheme.titleSmall!,
+                                  Get.theme.textTheme.titleSmall!,
                                   letterSpacing: 0.1,
-                                  color: themeData.colorScheme.onBackground,
+                                  color: Get.theme.colorScheme.onBackground,
                                   fontWeight: 500),
                               hintText: "Kata sandi",
-                              border: allTFBorder,
-                              enabledBorder: allTFBorder,
-                              focusedBorder: allTFBorder,
+                              border: controller.getOutlineInputBorder,
+                              enabledBorder: controller.getOutlineInputBorder,
+                              focusedBorder: controller.getOutlineInputBorder,
                               prefixIcon: Icon(
                                 MdiIcons.lockOutline,
                                 size: 22,
                               ),
                               suffixIcon: InkWell(
                                 onTap: () {
-                                  setState(() {
-                                    showPassword = !showPassword;
-                                  });
+                                    controller.showPassword.value = !(controller.showPassword.value);
                                 },
                                 child: Icon(
-                                  showPassword
+                                  (controller.showPassword.value)
                                       ? MdiIcons.eyeOffOutline
                                       : MdiIcons.eyeOutline,
                                   size: MySize.size22,
@@ -179,7 +106,7 @@ class _LoginScreenState extends State<LoginScreen> {
                               isDense: true,
                               contentPadding: Spacing.zero,
                             ),
-                            controller: passwordTFController,
+                            controller: controller.passwordTFController,
                             keyboardType: TextInputType.visiblePassword,
                           ),
                         ),
@@ -191,7 +118,7 @@ class _LoginScreenState extends State<LoginScreen> {
                                     Radius.circular(MySize.size48)),
                                 boxShadow: [
                                   BoxShadow(
-                                    color: themeData.colorScheme.primary
+                                    color: Get.theme.colorScheme.primary
                                         .withAlpha(100),
                                     blurRadius: 5,
                                     offset: Offset(0, 5),
@@ -199,22 +126,16 @@ class _LoginScreenState extends State<LoginScreen> {
                                 ],
                               ),
                               child: ElevatedButton(
-                                // shape: RoundedRectangleBorder(
-                                //     borderRadius:
-                                //         BorderRadius.circular(MySize.size8)),
-                                // color: themeData.colorScheme.primary,
-                                // highlightColor: themeData.colorScheme.primary,
-                                // splashColor: Colors.white.withAlpha(100),
-                                // padding: Spacing.only(top: 16, bottom: 16),
+                               
                                 onPressed:
-                                    isInProgress ? () {} : () => _handleLogin(),
+                                    controller.isInProgress.value ? () {} : () => controller.handleLogin(),
                                 child: Stack(
                                   // overflow: Overflow.visible,
                                   alignment: Alignment.center,
                                   children: <Widget>[
                                     Align(
                                       alignment: Alignment.center,
-                                      child: isInProgress
+                                      child: controller.isInProgress.value
                                           ? Container(
                                               width: MySize.size20,
                                               height: MySize.size20,
@@ -222,16 +143,16 @@ class _LoginScreenState extends State<LoginScreen> {
                                                 valueColor:
                                                     AlwaysStoppedAnimation<
                                                             Color>(
-                                                        themeData.colorScheme
+                                                        Get.theme.colorScheme
                                                             .onPrimary),
                                               ),
                                             )
                                           : Text(
                                               "MASUK".toUpperCase(),
                                               style: AppTheme.getTextStyle(
-                                                  themeData
+                                                  Get.theme
                                                       .textTheme.bodyMedium!,
-                                                  color: themeData
+                                                  color: Get.theme
                                                       .colorScheme.onPrimary,
                                                   letterSpacing: 0.8,
                                                   fontWeight: 700),
@@ -255,18 +176,17 @@ class _LoginScreenState extends State<LoginScreen> {
                               child: Text(
                                 "Ada Kendala? Klick Disini",
                                 style: AppTheme.getTextStyle(
-                                    themeData.textTheme.bodyMedium!,
-                                    color: themeData.colorScheme.onBackground,
+                                    Get.theme.textTheme.bodyMedium!,
+                                    color: Get.theme.colorScheme.onBackground,
                                     fontWeight: 500,
                                     decoration: TextDecoration.underline),
                               ),
                             ),
                           ),
                         ),
-                        // AuthController.notice(themeData)
+                        // AuthController.notice(Get.theme)
                       ],
-                    ))));
-      },
+                    )),
     );
   }
 
@@ -278,9 +198,9 @@ class _LoginScreenState extends State<LoginScreen> {
     //   SnackBar(
     //     duration: duration,
     //     content: Text(message,
-    //         style: AppTheme.getTextStyle(themeData.textTheme.subtitle2,
-    //             letterSpacing: 0.4, color: themeData.colorScheme.onPrimary)),
-    //     backgroundColor: themeData.colorScheme.primary,
+    //         style: AppTheme.getTextStyle(Get.theme.textTheme.subtitle2,
+    //             letterSpacing: 0.4, color: Get.theme.colorScheme.onPrimary)),
+    //     backgroundColor: Get.theme.colorScheme.primary,
     //     behavior: SnackBarBehavior.fixed,
     //   ),
     // );
